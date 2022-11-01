6d9e7dabf282832d2d81b8cd9df8ea479780efa5
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-11-01 11:50:41 +0900

    accounts/usermodel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
-# Create your models here.
+class User(AbstractUser):
+    followings = models.ManyToManyField("self", symmetrical=False, related_name="followers")
+    profile_image = ProcessedImageField(
+        upload_to="images/",
+        blank=True,
+        processors=[ResizeToFill(100, 100)],
+        format="JPEG",
+        options={"quality": 80},
+    )
+
+    @property
+    def full_name(self):
+        return f"{self.last_name}{self.first_name}"
