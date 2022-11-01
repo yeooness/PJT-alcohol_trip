7d5726d36158307bc8f06b474ec9e47c3254b659
황여원 <yeooness@naver.com>
2022-11-01 15:54:06 +0900

    reviews

--- bars/views.py
+++ bars/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 def index(request):
-    return render(request, 'bars/index.html')
+    return render(request, "bars/index.html")
+
 
 def detail(request):
-    return render(request, 'bars/detail.html')
+    return render(request, "bars/detail.html")
+
 
 def review(request):
-    return render(request, 'bars/review.html')
+    return render(request, "bars/review.html")
+
+
+def update(request):
+    return redirect("bars:review")
+
+
+def delete(request):
+    return redirect("bars:detail")
+
 
 def comment(request):
-    return render(request, 'bars/comment.html')
+    return render(request, "bars/comment.html")
+
 
 def like(request):
-    return render(request, 'bars/like.html')
+    return render(request, "bars/like.html")
