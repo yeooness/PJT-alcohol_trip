0cdb9df1be43e382ff9934e2e18601e9ffe1ecc9
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-11-03 14:59:40 +0900

    login_working

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('signin/', views.signin, name='signin'),
+    path('login/', views.login, name='login'),
 ]
--- alcohol_trip/urls.py
+++ alcohol_trip/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bars/', include('bars.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('userpage/', include('userpage.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
