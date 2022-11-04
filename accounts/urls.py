b4c0bc83930822c2ca73d5fc1c58c86602ecbd14
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-11-04 12:00:01 +0900

    accounts/userpage

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
     path('<int:pk>/', views.userpage, name='userpage'),
+    path('<int:pk>/follow/', views.follow, name='follow'),
     path('login/naver_callback/', views.naver_callback, name='naver_callback'),
     path('id_check/', views.id_check, name='id_check'),
+    path('id_check_naver/', views.id_check_naver, name='id_check_naver'),
 ]
